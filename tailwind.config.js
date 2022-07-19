56fb9e4cc7e65341aa1a8913d169d2c6a7436391
RansomBroker <yadisoke@gmail.com>
2022-07-19 16:58:53 +0700

    add some changes

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./resources/views/*.blade.php", "./resources/views/includes/*.blade.php"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
